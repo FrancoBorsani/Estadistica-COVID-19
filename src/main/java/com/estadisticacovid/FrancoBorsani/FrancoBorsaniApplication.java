e97eea9f3ff7765943757d0952eb2fc7317c4abd
unknown <borsafranco@gmail.com>
2020-07-29 14:48:55 -0300

    Proyecto base con algunas dependencias

---
+++ src/main/java/com/estadisticacovid/FrancoBorsani/FrancoBorsaniApplication.java
@@ -0,0 +1,13 @@
+package com.estadisticacovid.FrancoBorsani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrancoBorsaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrancoBorsaniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estadisticacovid/FrancoBorsani/FrancoBorsaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estadisticacovid.FrancoBorsani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrancoBorsaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
