3b77566d4376d9bcad84930be2ad6cd1a9ceb42c
unknown <borsafranco@gmail.com>
2020-07-31 18:22:38 -0300

    Entidades User y UserRole

---
+++ src/main/java/com/estadisticacovid/FrancoBorsani/entities/User.java
@@ -0,0 +1,89 @@
+package com.estadisticacovid.FrancoBorsani.entities;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name = "user")
+public class User{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	
+	@Column(name="username", unique=true, nullable=false, length=45)
+	private String username;
+	
+	@Column(name="password", nullable=false, length=60)
+	private String password;
+	
+	@Column(name = "enabled")
+	private boolean enabled;
+	
+	@OneToMany(fetch=FetchType.LAZY, mappedBy = "user")
+	private Set <UserRole>	userRoles = new HashSet<UserRole>();
+	
+	public User() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Set<UserRole> getUserRoles() {
+		return userRoles;
+	}
+
+	public void setUserRoles(Set<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
+	
+	
+
+
+	
+	
+	
+}
